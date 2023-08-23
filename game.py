c7ff45d91efd8cd5af40d8aee1a3eec9d67f81ed
Char1iee <47782807+Char1iee@users.noreply.github.com>
2023-08-23 16:04:01 +0800

    Add files via upload
    functions completed:
    1. abstract class Enemy and its implementation Enemy1,2,3
    2. move collision functions to Bullet class and Character to make the code more logical
    functions in progress:
    none
    functions planned:
    1. user interface
    2. ammo number
    3. different types of ammo
    4. blood system

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import time
 import random
+from abc import ABC, abstractclassmethod
+
 
 pygame.init()
 
@@ -36,25 +38,36 @@ class Character:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.me = pygame.Surface((20,20))
+        self.size = 20
+        self.me = pygame.Surface((self.size,self.size))
         self.me.fill((140,200,13))
         self.x_speed = 0
         self.y_speed = 0
 
-    def move(self,instruction):
+    def move(self):
         self.x += self.x_speed
         self.y += self.y_speed
 
     def check_border(self):
         if self.x <= 0:
             self.x = 0
-        if (self.x+20) >= display_width:
-            x = display_width - 20
+        if (self.x+self.size) >= display_width:
+            x = display_width - self.size
         if(self.y <= 0):
             self.y = 0
-        if (self.y+20) >= display_height:
-            self.y = display_height - 20
+        if (self.y+self.size) >= display_height:
+            self.y = display_height - self.size
 
+    def check_collision(self,object):
+        collide = False
+        if isinstance(object,Enemy):
+            if ((object.x >= self.x and object.x <= self.x+self.size or 
+                object.x+object.size >= self.x and object.x+object.size <= self.x+self.size) and
+                (object.y <= self.y+self.size and object.y >= self.y or
+                object.y+self.size >= self.y and object.y+object.size <= self.y+self.size)):
+                collide = True
+
+        return collide
 
 # probably enemy does not need check collision but bullet and character do
 # the logic is that enemy does not need to care if they hit something because they have highest priority
@@ -65,72 +78,66 @@ class Bullet:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.length = 7
+        self.height = 12
         self.b = pygame.Surface((7,12))
     
     def move(self):
         self.y -= 5
 
-class Enemy:
+    def check_collision(self,object):
+        collide = False
+        if isinstance(object,Enemy):
+            if (((self.x >= object.x and self.x <= object.x+object.size) or 
+                (self.x+self.length >= object.x and self.x+self.length <= object.x+object.size)) and
+                ((self.y <= object.y+object.size and self.y >= object.y) or 
+                (self.y+self.height >= object.y and self.y+self.height <= object.y+object.size))):
+                collide = True
+
+        return collide
+                        
+
+class Enemy(ABC):
+    @abstractclassmethod
+    def __init__(self,x,y):
+        pass
+
+    def move(self):
+        pass
+
+
+class Enemy1(Enemy):
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.e = pygame.Surface((30,30))
+        self.size = 30
+        self.e = pygame.Surface((self.size,self.size))
     
     def move(self):
         self.y += 7
 
-    def check_collision(self,object):
-        collide = False
-        if isinstance(object,Character):
-            if ((object.x >= self.x and object.x <= self.x+30 or 
-                object.x+20 >= self.x and object.x+20 <= self.x+30) and
-                (object.y <= self.y+30 and object.y >= self.y or
-                object.y+20 >= self.y and object.y+20 <= self.y+30)):
-                collide = True
-        else:
-            if ((object.x >= self.x and object.x <= self.x+30 or 
-                object.x+7 >= self.x and object.x+7 <= self.x+30) and
-                (object.y <= self.y+30 and object.y >= self.y or
-                object.y+12 >= self.y and object.y+12 <= self.y+30)):
-                collide = True
 
-        return collide
-
-class Enemy2: # enemy with larger size and faster speed
+class Enemy2(Enemy): # enemy with larger size and faster speed
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.e = pygame.Surface((50,50))
+        self.size = 50
+        self.e = pygame.Surface((self.size,self.size))
     
     def move(self):
         self.y += 10
 
-    def check_collision(self,object):
-        collide = False
-        if ((object.x >= self.x and object.x <= self.x+50 or 
-            object.x+20 >= self.x and object.x+20 <= self.x+50) and
-            (object.y <= self.y+50 and object.y >= self.y or
-            object.y+20 >= self.y and object.y+20 <= self.y+50)):
-            collide = True
-        return collide
 
-class Enemy3: # enemy with smaller size and faster speed
+class Enemy3(Enemy): # enemy with smaller size and faster speed
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.e = pygame.Surface((20,20))
+        self.size = 25
+        self.e = pygame.Surface((self.size,self.size))
     
     def move(self):
         self.y += 13
 
-    def check_collision(self,object):
-        collide = False
-        if ((object.x >= self.x and object.x <= self.x+30 or 
-            object.x+20 >= self.x and object.x+20 <= self.x+30) and
-            (object.y <= self.y+30 and object.y >= self.y or
-            object.y+20 >= self.y and object.y+20 <= self.y+30)):
-            collide = True
-        return collide
 
 def text_objects(text, font):
     textSurface = font.render(text, True, (0,0,0))
@@ -167,17 +174,17 @@ def game_loop():
                 exit()
 
             if event.type == enemy_timer:
-                if score < 20:
+                if score < 5:
                     e_x = random.randint(0,display_width-30)
                     e_y = 0
-                    e = Enemy(e_x,e_y)
+                    e = Enemy1(e_x,e_y)
                     enemies.append(e)
-                elif score < 30:
+                elif score < 10:
                     enemy_type = random.randint(0,2)
                     if enemy_type == 0:
                         e_x = random.randint(0,display_width-30)
                         e_y = 0
-                        e = Enemy(e_x,e_y)
+                        e = Enemy1(e_x,e_y)
                         enemies.append(e)
                     else:
                         e_x = random.randint(0,display_width-30)
@@ -189,7 +196,7 @@ def game_loop():
                     if enemy_type == 0:
                         e_x = random.randint(0,display_width-30)
                         e_y = 0
-                        e = Enemy(e_x,e_y)
+                        e = Enemy1(e_x,e_y)
                         enemies.append(e)
                     elif enemy_type == 1:
                         e_x = random.randint(0,display_width-30)
@@ -204,7 +211,7 @@ def game_loop():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    b = Bullet(x+3,y-12)
+                    b = Bullet(me.x+3,me.y-12)
                     bullets.append(b)
 
             if event.type == pygame.KEYUP:
@@ -231,7 +238,7 @@ def game_loop():
         
 
         screen.fill((156,156,156))
-        screen.blit(me,(x,y))
+        screen.blit(me.me,(me.x,me.y))
 
         for bullet in bullets:
             if bullet != None:
@@ -246,20 +253,14 @@ def game_loop():
                 screen.blit(enemy.e,(enemy.x,enemy.y))
                 # if enemy.y >= display_height:
                 #     enemies.remove(enemy)
-                if ((x >= enemy.x and x <= enemy.x+30 or 
-                        x+20 >= enemy.x and x+20 <= enemy.x+30) and
-                        (y <= enemy.y+30 and y >= enemy.y or
-                        y+20 >= enemy.y and y+20 <= enemy.y+30)):
-                            fail = True
-                            lose()
+                if me.check_collision(enemy):
+                    fail = True
+                    lose()
         
         for bullet in bullets:
             for enemy in enemies:
                 if bullet != None and enemy != None:
-                    if (((bullet.x >= enemy.x and bullet.x <= enemy.x+30) or 
-                        (bullet.x+7 >= enemy.x and bullet.x+7 <= enemy.x+30)) and
-                        ((bullet.y <= enemy.y+30 and bullet.y >= enemy.y) or 
-                        (bullet.y+12 >= enemy.y and bullet.y+12 <= enemy.y+30))):
+                    if bullet.check_collision(enemy):
                         bullets.remove(bullet)
                         enemies.remove(enemy)
                         score += 1
